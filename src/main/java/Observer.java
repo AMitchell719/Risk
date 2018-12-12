5da9a425c29294249dcab7c31a448c65066696e9
AMitchell719 <Aaron.Mitchell75@gmail.com>
2018-12-12 15:06:48 -0600

    Create Observer.java

---
+++ src/main/java/Observer.java
@@ -0,0 +1,9 @@
+/**
+ * The Observer interface is used as part of the Observer pattern to notify Users
+ * that a territory and it's occupying User are under attack
+ * @author Aaron Mitchell
+ * @version 0.5
+ */
+public interface Observer {
+    public void update();
+}
