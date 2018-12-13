af6cb451bfbd66a1b6ca8916ae2c4d9b2821298f
AMitchell719 <Aaron.Mitchell75@gmail.com>
2018-12-12 22:37:05 -0600

    Update DadBot.java

--- src/main/java/DadBot.java
+++ src/main/java/DadBot.java
@@ -25,7 +25,7 @@ public class DadBot extends TelegramLongPollingBot{
 
     /**
      * Method used by the bot to identify the commands users are inputting. It can start the game,
-     * add users to the game, attack territories, fortify territories and relay messages
+     * add users to the game, attack territories, and fortify territories
      * @param update This is the message users are entering into the telegram application
      * @see DadBot
      */
@@ -60,8 +60,6 @@ public class DadBot extends TelegramLongPollingBot{
 
                 // Spawn board, deck and Twitter
                 board.setupBoard();
-                //Deck deck = new Deck(board.getBoardTerritories());
-                //TweetPoster tp = new TweetPoster();
 
                 try {
                     execute(message); // Sending our message object to user
@@ -107,11 +105,7 @@ public class DadBot extends TelegramLongPollingBot{
             // Attack the specified territory
             else if(message_text.equals("/attack@TeamHALBot")){
                 SendMessage attack = new SendMessage().setChatId(chat_id).setText("Player 2, NorthWest Territory has lost 5 armies!");
-
-
                 board.getTerritoryName("Alaska").decrementArmies(1);
-                //board.getTerritoryName("NorthwestTerritory").getUser().removeArmyPower(5);
-                //System.out.println( board.getTerritoryName(container.get(1)).getArmyPower());
 
                 try {
                     execute(attack); // Sending our message object to user
@@ -164,20 +158,6 @@ public class DadBot extends TelegramLongPollingBot{
 
                 container.clear();
             }
-
-            // Otherwise copy the message and return it
-            else {
-                SendMessage message = new SendMessage() // Create a message object object
-                        .setChatId(chat_id)
-                        .setText(message_text);
-                try {
-                    execute(message); // Sending our message object to user
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-
-                container.clear();
-            }
         }
     }
 
@@ -210,134 +190,14 @@ public class DadBot extends TelegramLongPollingBot{
      * @see Board
      */
     public void setupTelegramTerritories(){
+        // Add player 1's territories to HashMap
         userList.get(0).addTerritory(board.getTerritoryName("Alaska"));
         board.setUserOccupant("Alaska", userList.get(0));
         board.getTerritoryName("Alaska").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("NorthwestTerritory"));
-        board.setUserOccupant("NorthwestTerritory", userList.get(0));
-        board.getTerritoryName("NorthwestTerritory").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("Greenland"));
-        board.setUserOccupant("Greenland", userList.get(0));
-        board.getTerritoryName("Greenland").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("Alberta"));
-        board.setUserOccupant("Alberta", userList.get(0));
-        board.getTerritoryName("Alberta").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("Ontario"));
-        board.setUserOccupant("Ontario", userList.get(0));
-        board.getTerritoryName("Ontario").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("Venezuela"));
-        board.setUserOccupant("Venezuela", userList.get(0));
-        board.getTerritoryName("Venezuela").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("Brazil"));
-        board.setUserOccupant("Brazil", userList.get(0));
-        board.getTerritoryName("Brazil").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("NorthAfrica"));
-        board.setUserOccupant("NorthAfrica", userList.get(0));
-        board.getTerritoryName("NorthAfrica").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("Egypt"));
-        board.setUserOccupant("Egypt", userList.get(0));
-        board.getTerritoryName("Egypt").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("EastAfrica"));
-        board.setUserOccupant("EastAfrica", userList.get(0));
-        board.getTerritoryName("EastAfrica").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("NorthernEurope"));
-        board.setUserOccupant("NorthernEurope", userList.get(0));
-        board.getTerritoryName("NorthernEurope").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("SouthernEurope"));
-        board.setUserOccupant("SouthernEurope", userList.get(0));
-        board.getTerritoryName("SouthernEurope").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("WesternEurope"));
-        board.setUserOccupant("WesternEurope", userList.get(0));
-        board.getTerritoryName("WesternEurope").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("Indonesia"));
-        board.setUserOccupant("Indonesia", userList.get(0));
-        board.getTerritoryName("Indonesia").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("NewGuinea"));
-        board.setUserOccupant("NewGuinea", userList.get(0));
-        board.getTerritoryName("NewGuinea").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("WesternAustralia"));
-        board.setUserOccupant("WesternAustralia", userList.get(0));
-        board.getTerritoryName("WesternAustralia").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("Siam"));
-        board.setUserOccupant("Siam", userList.get(0));
-        board.getTerritoryName("Siam").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("India"));
-        board.setUserOccupant("India", userList.get(0));
-        board.getTerritoryName("India").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("China"));
-        board.setUserOccupant("China", userList.get(0));
-        board.getTerritoryName("China").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("Mongolia"));
-        board.setUserOccupant("Mongolia", userList.get(0));
-        board.getTerritoryName("Mongolia").setArmyPower(1);
-        userList.get(0).addTerritory(board.getTerritoryName("Peru"));
-        board.setUserOccupant("Peru", userList.get(0));
-        board.getTerritoryName("Peru").setArmyPower(1);
 
-        // Add player 2's countries to HashMap
-
+        // Add player 2's territories to HashMap
         userList.get(1).addTerritory(board.getTerritoryName("Quebec"));
         board.setUserOccupant("Quebec", userList.get(1));
         board.getTerritoryName("Quebec").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("WesternUnitedStates"));
-        board.setUserOccupant("WesternUnitedStates", userList.get(1));
-        board.getTerritoryName("WesternUnitedStates").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("EasternUnitedStates"));
-        board.setUserOccupant("EasternUnitedStates", userList.get(1));
-        board.getTerritoryName("EasternUnitedStates").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("CentralAmerica"));
-        board.setUserOccupant("CentralAmerica", userList.get(1));
-        board.getTerritoryName("CentralAmerica").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("Argentina"));
-        board.setUserOccupant("Argentina", userList.get(1));
-        board.getTerritoryName("Argentina").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("Congo"));
-        board.setUserOccupant("Congo", userList.get(1));
-        board.getTerritoryName("Congo").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("SouthAfrica"));
-        board.setUserOccupant("SouthAfrica", userList.get(1));
-        board.getTerritoryName("SouthAfrica").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("Madagascar"));
-        board.setUserOccupant("Madagascar", userList.get(1));
-        board.getTerritoryName("Madagascar").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("Iceland"));
-        board.setUserOccupant("Iceland", userList.get(1));
-        board.getTerritoryName("Iceland").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("Scandinavia"));
-        board.setUserOccupant("Scandinavia", userList.get(1));
-        board.getTerritoryName("Scandinavia").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("Ukraine"));
-        board.setUserOccupant("Ukraine", userList.get(1));
-        board.getTerritoryName("Ukraine").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("GreatBritian"));
-        board.setUserOccupant("GreatBritian", userList.get(1));
-        board.getTerritoryName("GreatBritian").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("EasternAustralia"));
-        board.setUserOccupant("EasternAustralia", userList.get(1));
-        board.getTerritoryName("EasternAustralia").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("Japan"));
-        board.setUserOccupant("Japan", userList.get(1));
-        board.getTerritoryName("Japan").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("Irkutsk"));
-        board.setUserOccupant("Irkutsk", userList.get(1));
-        board.getTerritoryName("Irkutsk").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("Yakutsk"));
-        board.setUserOccupant("Yakutsk", userList.get(1));
-        board.getTerritoryName("Yakutsk").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("Kamchatka"));
-        board.setUserOccupant("Kamchatka", userList.get(1));
-        board.getTerritoryName("Kamchatka").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("Siberia"));
-        board.setUserOccupant("Siberia", userList.get(1));
-        board.getTerritoryName("Siberia").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("Afghanistan"));
-        board.setUserOccupant("Afghanistan", userList.get(1));
-        board.getTerritoryName("Afghanistan").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("Ural"));
-        board.setUserOccupant("Ural", userList.get(1));
-        board.getTerritoryName("Ural").setArmyPower(1);
-        userList.get(1).addTerritory(board.getTerritoryName("MiddleEast"));
-        board.setUserOccupant("MiddleEast", userList.get(1));
-        board.getTerritoryName("MiddleEast").setArmyPower(1);
     }
 }
